c8826d7e01d853e5c131e93499e0f4bcc790b4a0
Yaroslav Konoplov <eahome00@gmail.com>
2017-01-05 17:54:47 +0200

    Change Rails::Engine to Rails::Railtie and annotate ruby scripts.

--- lib/pswp-rails.rb
+++ lib/pswp-rails.rb
@@ -1,23 +1,25 @@
-require 'pswp-rails/version'
+# encoding: utf-8
+# frozen_string_literal: true
+
+require 'rails/railtie'
 
 module PhotoSwipe
-  class Engine < Rails::Engine
-  #   initializer 'sprockets.photoswipe', group: :all, after: 'sprockets.environment' do |app|
-  #     configure_assets app do |env|
-  #       env.precompile += %w( photoswipe.js photoswipe-ui-default.js photoswipe.scss )
-  #
-  #     end
-  #   end
-  #
-  # private
-  #   def configure_assets(app)
-  #     if config.respond_to?(:assets) && config.assets.respond_to?(:configure)
-  #       # Rails 4.x
-  #       config.assets.configure { |env| yield(env) }
-  #     else
-  #       # Rails 3.2
-  #       yield(app.assets)
-  #     end
-  #   end
+  class Railtie < Rails::Railtie
+    initializer 'sprockets.photoswipe', after: 'sprockets.environment' do |app|
+      configure_assets app do |env|
+        env.precompile += %w( photoswipe.js photoswipe-ui-default.js photoswipe.scss )
+      end
+    end
+
+  private
+    def configure_assets(app)
+      if config.respond_to?(:assets) && config.assets.respond_to?(:configure)
+        # Rails 4.x 5.x
+        config.assets.configure { |env| yield(env) }
+      else
+        # Rails 3.2
+        yield(app.assets)
+      end
+    end
   end
 end
--- pswp-rails.gemspec
+++ pswp-rails.gemspec
@@ -1,6 +1,8 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
 require File.expand_path('../lib/pswp-rails/version', __FILE__)
 
-# frozen_string_literal: true
 Gem::Specification.new do |s|
   s.name            = 'pswp-rails'
   s.version         = PhotoSwipe::VERSION
@@ -11,8 +13,6 @@ Gem::Specification.new do |s|
   s.homepage        = 'https://github.com/yivo/pswp-rails'
   s.license         = 'MIT'
 
-  s.executables     = `git ls-files -z -- bin/*`.split("\x0").map{ |f| File.basename(f) }
   s.files           = `git ls-files -z`.split("\x0")
-  s.test_files      = `git ls-files -z -- {test,spec,features}/*`.split("\x0")
   s.require_paths   = ['lib']
 end
